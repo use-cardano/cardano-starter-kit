81973975ac123b7f8f9ebac857e7d2ed084f3631
Jesper Tullberg <jesper.tullberg@zenta.se>
2022-12-22 17:04:54 +0100

    initial setup, install and configure tailwind and use-cardano

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import { CardanoProvider, CardanoToaster, UseCardanoOptions } from "use-cardano"
+import "use-cardano/styles/use-cardano.css"
+
+const options: UseCardanoOptions = {
+  allowedNetworks: ["Testnet"],
+  testnetNetwork: "Preview",
+  node: {
+    provider: "blockfrost-proxy",
+    proxyUrl: "/api/blockfrost",
+  },
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CardanoProvider options={options}>
+      <Component {...pageProps} />
+
+      <CardanoToaster />
+    </CardanoProvider>
+  )
 }
