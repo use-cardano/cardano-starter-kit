167918a886af19eedd9d0c1fb5ca17ebe60cd94c
Jesper Tullberg <jesper.tullberg@zenta.se>
2022-12-26 18:13:02 +0100

    show active navigation in menu

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,32 +1,39 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { useMemo } from "react"
+import { twMerge } from "tailwind-merge"
 import { CardanoWalletSelector } from "use-cardano"
 
+const className =
+  "h-10 text-white font-bold tracking-widest uppercase rounded mr-2 px-6 flex items-center bg-blue-300 shadow shadow-blue-100s hover:shadow-none hover:bg-blue-400 transition-all duration-300"
+
 export const Navigation = () => {
+  const { asPath } = useRouter()
+
+  console.log(asPath)
+
+  const isHome = useMemo(() => asPath === "/", [asPath])
+  const isTransact = useMemo(() => asPath === "/transact", [asPath])
+  const isSign = useMemo(() => asPath === "/sign", [asPath])
+  const isMint = useMemo(() => asPath === "/mint", [asPath])
+
   return (
     <nav className="flex h-24 items-center justify-between w-full py-2">
       <div className="flex items-center">
         <Link href="/">
-          <button className="h-10 text-white font-bold tracking-widest uppercase rounded mr-2 px-6 flex items-center bg-blue-300 shadow shadow-blue-100s hover:shadow-none hover:bg-blue-400 transition-all duration-300">
-            home
-          </button>
+          <button className={twMerge(className, isHome && "text-blue-500")}>home</button>
         </Link>
 
         <Link href="/transact">
-          <button className="h-10 text-white font-bold tracking-widest uppercase rounded mr-2 px-6 flex items-center bg-blue-300 shadow shadow-blue-100s hover:shadow-none hover:bg-blue-400 transition-all duration-300">
-            transact
-          </button>
+          <button className={twMerge(className, isTransact && "text-blue-500")}>transact</button>
         </Link>
 
         <Link href="/sign">
-          <button className="h-10 text-white font-bold tracking-widest uppercase rounded mx-2 px-6 flex items-center bg-blue-300 shadow shadow-blue-100s hover:shadow-none hover:bg-blue-400 transition-all duration-300">
-            Sign
-          </button>
+          <button className={twMerge(className, isSign && "text-blue-500")}>Sign</button>
         </Link>
 
         <Link href="/mint">
-          <button className="h-10 text-white font-bold tracking-widest uppercase rounded mx-2 px-6 flex items-center bg-blue-300 shadow shadow-blue-100s hover:shadow-none hover:bg-blue-400 transition-all duration-300">
-            Mint
-          </button>
+          <button className={twMerge(className, isMint && "text-blue-500")}>Mint</button>
         </Link>
       </div>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,24 +10,36 @@ export default function Home() {
       </h1>
 
       <div style={inter.style} className="my-4 text-left ">
-        This is a starter project for use-cardano, which is a React hook, context, and set of components
-        for interacting with the Cardano blockchain. It is built on top of{" "}
+        This is a starter project for use-cardano, which is a React hook, context, and set of
+        components for interacting with the Cardano blockchain. It is built on top of{" "}
         <a
           className="underline underline-offset-2"
-          href="https://www.npmjs.com/lucid-cardano"
+          href="https://lucid.spacebudz.io/"
           rel="noreferrer"
           target="_blank"
         >
           Lucid
         </a>{" "}
-        and provides a simple API for interacting with the Cardano blockchain. It is designed to be
-        easy to use, while still providing all the bells and whistles you need to build your dApp.
+        and provides a simple API for interacting with the Cardano blockchain.
       </div>
 
       <div style={inter.style} className="my-4 text-left">
-        The aim of use-cardano in general, and the starter project specifically, is to serve as a simple
-        starting point, but also to help developers get into Cardano dApp development. With this in
-        mind, the examples are designed to be equally functional and educational.
+        The aim of use-cardano in general, and the starter project specifically, is to serve as a
+        simple starting point, but also to help developers get into Cardano dApp development. With
+        this in mind, the examples are designed to be equally functional and educational.
+      </div>
+
+      <div style={inter.style} className="my-4 text-left">
+        The starter project is built on top of Next.js, which is a React framework for building
+        static and dynamic websites. It is a great choice for building dApps, as it is easy to use,
+        and provides a lot of functionality out of the box. It also has a great developer
+        experience, which makes it easy to get started.
+      </div>
+
+      <div style={inter.style} className="my-4 text-left">
+        For styling, we are using Tailwind CSS, which is a utility-first CSS framework with a good
+        balance of flexibility, composability, and ease of use. It is also very easy to use with
+        Next.js, and provides a lot of functionality out of the box.
       </div>
     </div>
   )
